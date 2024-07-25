9bd73e5403032ff8347bf664c501cc0f0af21a19
Mike Slinn <mslinn@mslinn.com>
2024-07-25 15:33:43 -0400

    -

--- lib/jekyll_video.rb
+++ lib/jekyll_video.rb
@@ -5,7 +5,7 @@ require 'jekyll_plugin_support'
 require_relative 'jekyll_video/version'
 
 # Require all Ruby files in 'lib/', except this file
-Dir[File.join(__dir__, '*.rb')].each do |file|
+Dir[File.join(__dir__, '*.rb')].sort.each do |file|
   require file unless file.end_with?('/jekyll_video.rb')
 end
 
